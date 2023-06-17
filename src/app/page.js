7b496004c56aa14ca56eba3986d9d832e658ee9f
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-06-17 01:51:07 -0400

    home

--- src/app/page.js
+++ src/app/page.js
@@ -12,23 +12,23 @@ export default async function Home() {
   const produtos = await req.json();
 
   return (
-    <main  style={{}}>
+    <main className='geral'>
     <Navbar></Navbar>
 
     <div className="flex justify-center items-center w-100">
       <Carrossel />
     </div>
 
-    <div className="grid grid-cols-3 gap-5">
+    <div className="grid grid-cols-4 gap-5">
 
       {produtos.map(produtos => (
-        <div className=""  key={produtos.id}>
-          <h1  className="font-bold text-3xl pb-10 underline">{produtos.titulo}</h1>
-          <p> {produtos.descricao}</p>
-          <img src={produtos.imagem}/>
-          <p>{produtos.preco}</p>
+        <div className="produto"  key={produtos.id}>
+          <h1  className="font-bold text-2xl">{produtos.titulo}</h1>
+          <p className='text-lg mt-2 mb-4'> {produtos.descricao}</p>
+          <img className='w-1/2 h-96' src={produtos.imagem}/>
+          <p className='font-semibold text-lg'>R$ {produtos.preco}</p>
           <p>{produtos.dt_cad}</p>
-          <Link href={`/produto/${produtos.id}`}>ver mais</Link>
+          <Link className='font-medium text-blue-600 dark:text-blue-500 hover:underline' href={`/produto/${produtos.id}`}>Ver mais</Link>
         
         </div>
       ))}
